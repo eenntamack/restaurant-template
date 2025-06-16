4434846e1d1448ad21522401728c34d2dc03024c
entamack1511 <entamack1511@gmail.com>
2025-06-16 12:50:47 -0400

    updated the schema to account for whether the guest want to order from home,in person and the type of food they request

--- restaurant-template-backend/schemas/guests.js
+++ restaurant-template-backend/schemas/guests.js
@@ -2,9 +2,17 @@
 export const guests = new mongoose.Schema({
     name:{type:String, required:true},
     status:{type:String,emun:['important','caution','banned','standard']},
+    location:{
+        type:String,
+        enum:["Remote","In-person"]
+    },
     reservationDay:{
         type:String,
         enum:["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
     },
-    reservationTime:{type:Date}
+    reservationTime:{type:Date},
+    dish:[{
+        type:String,
+        reuired:true
+    }]
 })

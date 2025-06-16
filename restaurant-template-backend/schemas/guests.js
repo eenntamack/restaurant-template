2b7a2a382fced44eef54a1ad1a244e937e312b83
entamack1511 <entamack1511@gmail.com>
2025-06-16 12:47:05 -0400

    updated the schema for guests, accouting for their registeration times

--- restaurant-template-backend/schemas/guests.js
+++ restaurant-template-backend/schemas/guests.js
@@ -1,5 +1,10 @@
 
 export const guests = new mongoose.Schema({
     name:{type:String, required:true},
-    status:{type:String,emun:['important','caution','banned']},
+    status:{type:String,emun:['important','caution','banned','standard']},
+    reservationDay:{
+        type:String,
+        enum:["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    },
+    reservationTime:{type:Date}
 })
